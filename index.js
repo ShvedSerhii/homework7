4499396eb458e1f542e0d28c779d140c94a3f2d3
Serhii <chuhaystyr@gmail.com>
2019-12-16 21:51:51 +0200

    homework7

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const { fromEvent } = rxjs
 const { debounceTime, map } = rxjs.operators
 
-const old = console.log
-const logger = document.getElementById("log")
-console.log = function(message) {
+const logger = document.getElementById('log')
+const button = document.getElementById('test-button')
+
+function print(message) {
   logger.innerHTML +=
-    "<span style='color:red'>time: </span>" + message + "<br />"
-  old(message)
+    '<span style="color:red">time: </span>' + message + '<br />'
 }
 
-const button = document.getElementById("test-button")
-const clicks = fromEvent(button, "click")
+const clicks = fromEvent(button, 'click')
 const result = clicks.pipe(
   map(() => new Date().toTimeString()),
   debounceTime(1000)
 )
-result.subscribe(x => console.log(x))
+result.subscribe(x => print(x))
